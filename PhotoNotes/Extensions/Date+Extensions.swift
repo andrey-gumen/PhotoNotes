bbe65e2ef985da8ec5f30ea5d8d29f4e2e08ec5f
a.gumen <a.gumen@revizto.com>
2022-12-19 00:57:50 +0100

    add Date extension

---
+++ PhotoNotes/Extensions/Date+Extensions.swift
@@ -0,0 +1,34 @@
+import Foundation
+
+extension Date {
+    
+    static func randomBetween(start: Date, end: Date) -> Date {
+        var date1 = start
+        var date2 = end
+        if date2 < date1 {
+            let temp = date1
+            date1 = date2
+            date2 = temp
+        }
+        
+        let span = TimeInterval.random(in: date1.timeIntervalSinceNow...date2.timeIntervalSinceNow)
+        return Date(timeIntervalSinceNow: span)
+    }
+
+    func getFormattedDate(_ format: String) -> String {
+        let formatter = Date.getFormatter(format)
+        return formatter.string(from: self)
+    }
+    
+    static let itemFormatter: DateFormatter = {
+        return getFormatter()
+    }()
+    
+    private static func getFormatter(_ format: String = "dd MMM yyyy") -> DateFormatter {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .long
+        formatter.dateFormat = format
+        return formatter
+    }
+
+}
