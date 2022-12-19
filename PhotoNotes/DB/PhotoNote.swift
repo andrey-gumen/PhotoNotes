75a81cf8822f070d82bf7201ce7d621ec7bf5300
a.gumen <a.gumen@revizto.com>
2022-12-19 01:30:11 +0100

    update PhotoNote structure

--- PhotoNotes/DB/PhotoNote.swift
+++ PhotoNotes/DB/PhotoNote.swift
@@ -5,9 +5,9 @@ struct PhotoNote: Identifiable {
     let id: UUID = UUID()
     
     // MARK: required properties
-    let date: Date
+    var date: Date
     
     // MARK: optional properties
-    let image: UIImage?
-    let note: String?
+    var image: UIImage?
+    var note: String?
 }
