a86b05a0bc359d42ecdf2db506dc12cbde5b5266
a.gumen <a.gumen@revizto.com>
2022-12-23 21:16:55 +0100

    add preview for app navigation view

--- PhotoNotes/App/AppNavigationView.swift
+++ PhotoNotes/App/AppNavigationView.swift
@@ -6,10 +6,18 @@ struct AppNavigationView: View {
     @StateObject var rootCoordinator: AppCoordinator
     
     var body: some View {
-        NavigationView {
+        NavigationStack {
             let coordinator = GalleryCoordinator(rootCoordinator.persistenceController)
             coordinator.start()
         }
+        .navigationViewStyle(.stack)
     }
 
+}
+
+struct AppNavigationView_Previews: PreviewProvider {
+    static var previews: some View {
+        let rootCoordinator =  AppCoordinator(PersistenceController.preview)
+        AppNavigationView(rootCoordinator: rootCoordinator)
+    }
 }
