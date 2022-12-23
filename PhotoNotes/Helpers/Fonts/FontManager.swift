6c1f5dfc0ae56fc6d0d27d601c6085b5a753f2da
a.gumen <a.gumen@revizto.com>
2022-12-23 13:05:57 +0100

    add fonts

---
+++ PhotoNotes/Helpers/Fonts/FontManager.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+final class FontManager {
+    
+    static let manropeRegular14 = UIFont(name: "Manrope-Regular", size: 14) ?? UIFont.systemFont(ofSize: 14)
+    static let manropeBold14 = UIFont(name: "Manrope-Bold", size: 14) ?? UIFont.systemFont(ofSize: 14, weight: .bold)
+    
+}
